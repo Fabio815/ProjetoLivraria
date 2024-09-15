0dd55300f5050a27d5153036256f9e84d1ab2392
Fábio <fabiomotamarques815@gmail.com>
2024-09-15 18:11:15 -0300

    Adicionado as classes

---
+++ ProjetoLivraria/Modelos/Autor.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjetoLivraria.Modelos;
+
+internal class Autor
+{
+    List<Livros> listaLivro = new List<Livros>();
+    public int IdAutor { get; set; }
+    public string Nome { get; set; }
+    public string Nacionalidade { get; set; }
+
+    public void AdicionarLivrosAoAutor(Livros livros)
+    {
+        listaLivro.Add(livros);
+    }
+}
---
+++ ProjetoLivraria/Modelos/Livros.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoLivraria.Modelos;
+
+internal class Livros
+{
+    public int IdLivro { get; set; }
+    public string Titulo { get; set; }
+    public string Genero { get; set; }
+    public int Quantidade { get; set; }
+    public int AnoLancamento { get; set; }
+}
