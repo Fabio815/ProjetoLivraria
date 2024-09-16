cabf2d6f7b1ae6df5c3f77ed09ee0ccfd502ca0f
Fabio815 <fabiomotamarques815@gmail.com>
2024-09-16 11:23:44 -0300

    Criando o listar livros

---
+++ ProjetoLivraria/Banco/ListarDAL.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using ProjetoLivraria.Modelos;
+
+namespace ProjetoLivraria.Banco;
+
+internal class ListarDAL
+{
+    public IEnumerable<Livros> Listar()
+    {
+        var listaLivros = new List<Livros>();
+        //Abrindo conexão com o banco de dados.
+        using var abrirConexao = new Conexao().AbrirConexao();
+        abrirConexao.Open();
+
+        string sql = "SELECT * FROM Livros";
+        SqlCommand comando = new SqlCommand(sql, abrirConexao);
+        using SqlDataReader leitura = comando.ExecuteReader();
+    }
+}
