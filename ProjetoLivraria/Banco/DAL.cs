732b9b3cf62a90187fd5d29bc8b6a1c49878c128
Fabio815 <fabiomotamarques815@gmail.com>
2024-09-28 21:02:32 -0300

    Criado o DAL

---
+++ ProjetoLivraria/Banco/DAL.cs
@@ -0,0 +1,43 @@
+﻿using ProjetoLivraria.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoLivraria.Banco
+{
+    //O tipo de referência vai ser uma classe.
+    internal abstract class DAL<T> where T: class
+    {
+        //Estou estabelecendo a conexão aqui na classe DAL.
+        protected readonly LivrariaContext context;
+        
+        protected DAL(LivrariaContext conexao)
+        {
+            this.context = conexao;
+        }
+        
+        public IEnumerable<T> Listar()
+        {
+            //Nesse método de listar estou usando a Set para pegar o tipo de referência.
+            return context.Set<T>().ToList();
+        }
+        public void AdicionarLivro(T objeto)
+        {
+            //A mesma coisa que acima, só que vou adicionar um objeto.
+            context.Set<T>().Add(objeto);
+            context.SaveChanges();
+        }
+        public void Atualizar(T objeto)
+        {
+            context.Set<T>().Update(objeto);
+            context.SaveChanges();
+        }
+        public void Deletar(T objeto)
+        {
+            context.Remove(objeto);
+            context.SaveChanges();
+        }
+    }
+}
--- ProjetoLivraria/Banco/LivrosDAL.cs
+++ ProjetoLivraria/Banco/LivrosDAL.cs
@@ -4,69 +4,10 @@ using ProjetoLivraria.Modelos;
 
 namespace ProjetoLivraria.Banco;
 
-internal class LivrosDAL
+internal class LivrosDAL : DAL<Livros>
 {
     //Estou fazendo com que seja aberto a conexão atráves desse construtor.
-    private readonly LivrariaContext context;
-    public LivrosDAL(LivrariaContext conexao)
-    {
-        this.context = conexao;
-    }
-
-    public IEnumerable<Livros> Listar()
-    {     
-        return context.Livros.ToList();
-
-        /*string sql = "SELECT * FROM Livros";
-        SqlCommand comando = new SqlCommand(sql, abrirConexao);
-        using SqlDataReader leitura = comando.ExecuteReader();
-        //Nessa parte estou colocando os dados em variáveis.
-        while(leitura.Read())
-        {
-            string titulo = Convert.ToString(leitura["Titulo"]);
-            string genero = Convert.ToString(leitura["Genero"]);
-            int quantidade = Convert.ToInt32(leitura["Quantidade"]);
-            int anoLancamento = Convert.ToInt32(leitura["AnoLancamento"]);
-            int idLivros = Convert.ToInt32(leitura["IdLivro"]);
-            //Agora estou colocando elas em uma variáveis, para adicionar na lista de livros que declarei no começo.
-            Livros livros = new(titulo, genero, quantidade, anoLancamento) { IdLivro = idLivros};
-
-            listaLivros.Add(livros);
-        }
-
-        return listaLivros;*/
-    }
-
-    public void AdicionarLivro(Livros livro)
-    {
-        context.Livros.Add(livro);
-        context.SaveChanges();
-
-        /*string sql = "INSERT INTO Livros(Titulo, Genero, Quantidade, AnoLancamento) VALUES (@titulo, @genero, @quantidade, @lancamento)";
-        SqlCommand comando = new SqlCommand(sql, abrirConexao);
-
-        comando.Parameters.AddWithValue("@titulo", livro.Titulo);
-        comando.Parameters.AddWithValue("@genero", livro.Genero);
-        comando.Parameters.AddWithValue("@quantidade", livro.Quantidade);
-        comando.Parameters.AddWithValue("@lancamento", livro.AnoLancamento);
-
-        int retorno = comando.ExecuteNonQuery();
-        Console.WriteLine(retorno);*/
-    }
-
-    public void Atualizar(Livros livros)
-    {
-        context.Update(livros);
-        context.SaveChanges();
-
-
-    }
-
-    public void Deletar(Livros livro)
-    {
-        context.Remove(livro);
-        context.SaveChanges();
-    }
+    public LivrosDAL(LivrariaContext conexao) : base(conexao) { }
 
     public Livros? EncontrarLivro(string livroProcurado)
     {
